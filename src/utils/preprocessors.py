45f31948b3cb0dd1035f962064744b09dc2d33d5
Kartheek Challa <kartheek.challa@tide.co>
2022-02-15 20:37:49 +0530

    One More test added

--- src/utils/preprocessors.py
+++ src/utils/preprocessors.py
@@ -12,3 +12,20 @@ class DropUnecessaryFeatures(BaseEstimator,TransformerMixin):
         X=X.copy()
         X=X.drop(self.variables,axis=1)
         return X
+
+class TemporalVariableEstimator(BaseEstimator,TransformerMixin):
+    def __init__(self,variables=None,reference_variable=None):
+        if not isinstance(variables,list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
+        self.reference_variables = reference_variable
+    
+    def fit(self,X,y=None):
+        return self
+    
+    def transform(self,X):
+        X = X.copy()
+        for feature in self.variables:
+            X[feature] = X[self.reference_variables] - X[feature]
+        return X
--- tests/test_preprocessors.py
+++ tests/test_preprocessors.py
@@ -11,3 +11,24 @@ def test_drop_unneccessary_features_transformer(pipeline_inputs):
     X_transformed = transformer.transform(X_train)
     #Then
     assert drop_features not in X_transformed.columns
+
+
+def test_temporal_variable_estimator(pipeline_inputs):
+    #Given
+    X_train,X_test,y_train,y_test = pipeline_inputs
+    config = read_yaml('config/config.yml')
+    temporal_vars = config['temporal_vars']
+    drop_features = config['drop_features']
+    transformer = pp.TemporalVariableEstimator(
+        variables=temporal_vars,
+        reference_variable = drop_features
+    )
+
+    #When
+    X_transformed = transformer.transform(X_train)
+
+    #Then
+    assert(
+        X_transformed.iloc[0]["YearRemodAdd"]
+        == X_train.iloc[0]["YrSold"] - X_train.iloc[0]["YearRemodAdd"]
+    )
